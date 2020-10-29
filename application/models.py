11885aec8e7da7d02191a90c291d3a2f67a3c4cf
Vladislav Tsarenko <vadiktsarenko@mail.ru>
2020-10-29 23:05:28 +0300

    create Index page

--- application/models.py
+++ application/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
--- hire/urls.py
+++ hire/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import re_path
+
+from application import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index),
 ]
