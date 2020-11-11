8c0be8e2e2fe4c62a3f9d9e62635dfa87429512b
Vladislav Tsarenko <vadiktsarenko@mail.ru>
2020-11-11 23:07:34 +0300

    basic authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,39 @@
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import LoginForm
 
-# Create your views here.
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect("/")
+                else:
+                    return render(request, 'registration/login.html', {'form': form, 'message': 'Неактивный аккаунт'})
+            else:
+                return render(request, 'registration/login.html',
+                              {
+                                'form': form,
+                                'message': 'Не удалось войти. Убедитесь что ввели верные Логин и Пароль'
+                              })
+    else:
+        form = LoginForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+
+def user_logout(request):
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            logout(request)
+    return HttpResponseRedirect("/")
+
+
+
--- hire/urls.py
+++ hire/urls.py
@@ -13,15 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
-from django.urls import re_path
 
-from application.views import product_views
+from application import views
+from accounts import views as account_views
 
 urlpatterns = [
-    path('', product_views.index),
-    path('edit-product', product_views.edit_product),
-    path('save-product', product_views.save_product),
-    path('delete-product', product_views.delete_product),
+    path('', views.index),
+    path('edit-product', views.edit_product),
+    path('save-product', views.save_product),
+    path('delete-product', views.delete_product),
+    path('login/', account_views.user_login, name='login'),
+    path('logout/', account_views.user_logout, name='logout'),
+    path('admin/', admin.site.urls),
 ]
